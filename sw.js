80fc44f29acb23da409b29fbd7c5f58aea0d6270
yanshuqi <yanshuqi1208@outlook.com>
2026-03-20 15:46:21 +0800

    更新缓存版本号强制刷新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'second-brain-v1';
+const CACHE_NAME = 'second-brain-v2';
 const ASSETS = ['./', './index.html', './style.css', './data.js', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
